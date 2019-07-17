513595f50d299d65fc8dda17ce28d13d111bdc0f
Diablo9946 <diabloch9946@gmail.com>
2019-07-17 17:07:55 +0200

    Email 3.

--- action.js
+++ action.js
@@ -3,13 +3,13 @@ var stampa, email, insemail;
 stampa = document.getElementById('my');
 
 email = ["marco@gmail.com", "paolo@gmail.com", "simone@gmail.com"];
-console.log("email");
+console.log("Email delle persone invitate sono" + email);
 
 insemail = prompt("Inserisci la Tua email.");
-console.log("insemail");
+console.log("La email scelta è " + insemail);
 
-for (var i=0, i < email.length, i++) {
-  if (insemail == email) {
+for (var i=0; i < email.length; i++) {
+  if (insemail === email) {
     stampa.innerHtml = ("Sei stato invitato")
   } else {
     stampa.innerHtml = ("Ci dispiace non sei stato selezionato")
--- action2.js
+++ action2.js
@@ -1,1 +1,6 @@
-document.getElementById('my');
+var stampa;
+
+stampa = document.getElementById('my');
+
+var num1 = Math.floor(Marh.randoom());
+var num2 = Math.floor(Marh.randoom());
