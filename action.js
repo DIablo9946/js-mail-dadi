b3cebf999495a2ddde3ede05789097699a2fff39
Diablo9946 <diabloch9946@gmail.com>
2019-07-17 17:00:51 +0200

    Email2.

--- action.js
+++ action.js
@@ -3,8 +3,10 @@ var stampa, email, insemail;
 stampa = document.getElementById('my');
 
 email = ["marco@gmail.com", "paolo@gmail.com", "simone@gmail.com"];
+console.log("email");
 
 insemail = prompt("Inserisci la Tua email.");
+console.log("insemail");
 
 for (var i=0, i < email.length, i++) {
   if (insemail == email) {
