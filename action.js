49c4c7a256217fbe83dc7f6ef5937b70c93e38c0
Diablo9946 <diabloch9946@gmail.com>
2019-07-18 07:17:56 +0200

    Before the rewriting of the first js code.

--- action.js
+++ action.js
@@ -8,8 +8,8 @@ console.log("Email delle persone invitate sono " + email);
 insemail = prompt("Inserisci la Tua email.");
 console.log("La email da Te scelta è " + insemail);
 
-for (var i=0; i < email.length; i++) {
-  if (insemail == email) {
+for (var i = 0; i < email.length; i++) {
+  if (insemail == email[i]) {
     stampa.innerHTML = "Quindi sei stato invitato";
   } else {
     stampa.innerHTML = "Ci dispiace non sei stato selezionato";
